be122c50ac1bc20ce2a3c14f72365c7bd7798ef5
Gabriela <gabyplasencia1995@gmail.com>
2024-01-26 11:05:49 +0000

    styles for menu options

--- js/music.js
+++ js/music.js
@@ -1,14 +1,20 @@
 const backgroundMusic = document.getElementById('musicJS');
 const musicOptions = document.querySelectorAll('.musicJS');
+const iconOn = document.getElementById('icon-on');
+const iconOff = document.getElementById('icon-off');
 
 backgroundMusic.volume = 0.1;
 
 musicOptions.forEach( option => {
     option.addEventListener('click', () => {
         if(option.id == "off"){
-            backgroundMusic.pause();           
+            backgroundMusic.pause(); 
+            iconOn.style.display = "none";
+            iconOff.style.display = "block";          
         }else if(option.id == "on"){
             backgroundMusic.play();
+            iconOff.style.display = "none";
+            iconOn.style.display = "block";
         }
     })
 })
